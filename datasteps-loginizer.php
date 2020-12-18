1d260c806e3979cc96a44fbeb6c3c1353b2aeb97
Christos Chiotis <support@elegrad.com>
2020-12-18 19:02:31 +0200

    Update datasteps-loginizer.php

--- datasteps-loginizer.php
+++ datasteps-loginizer.php
@@ -26,7 +26,22 @@ add_filter( 'login_headerurl', 'ds_login_url' );
 
 // changing the alt text on the logo to show your site name
 function ds_login_title() { return get_option( 'blogname' ); }
-add_filter( 'login_headertitle', 'ds_login_title' );
+add_filter( 'login_headertext', 'ds_login_title' );
+
+// Force WordPress to accept .doc files.
+add_filter( 'upload_mimes', function( $mime_types ) {
+    $mime_types['docx'] = 'Microsoft Word'; // Adding .docx extension
+    return $mime_types;
+}, 1, 1 );
+ 
+add_filter( 'wp_check_filetype_and_ext', function( $mime, $file, $filename, $mimes ) {
+    $wp_filetype = wp_check_filetype( $filename, $mimes );
+    if ( in_array( $wp_filetype['ext'], [ 'docx' ] ) ) {
+        $mime['ext']  = true;
+        $mime['type'] = true;
+    }
+    return $mime;
+}, 10, 4 );
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ){
