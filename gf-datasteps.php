df27782559c28dd79f63ad15319df54a5130d995
Christos Chiotis <chiotis@gmail.com>
2020-12-18 18:23:56 +0200

    Update and rename wp-smtp.php to gf-datasteps.php

---
+++ gf-datasteps.php
@@ -0,0 +1,11 @@
+<?php
+/** 
+* Plugin Name: GF Datasteps
+* Description: A customization plugin for Gravity Family
+* Author: Christos Chiotis
+* Author URI: https://elegrad.com
+* Version: 1.0.0
+*/
+
+// Style customizations for Gravity Flow
+require_once( dirname( __FILE__ ) . '/datasteps-styles.php' );
